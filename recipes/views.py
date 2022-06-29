9c09f33766c787fd269e77284fdfd3d0f61c9757
DaveCaulfield <dave.caulfield@gmail.com>
2022-06-29 17:42:20 +0000

    create and extend base.html to index.html and add url

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Recipe
 
-Class RecipeList(generic.ListView):
+
+class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
