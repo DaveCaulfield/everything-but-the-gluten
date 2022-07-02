4f26bbe4a035a8a28dd54023c71b081de3f372da
DaveCaulfield <dave.caulfield@gmail.com>
2022-07-02 16:13:12 +0000

    add allauth signup, login, logout features with redirect to homepage

--- everything_but_the_gluten/urls.py
+++ everything_but_the_gluten/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('recipes.urls'), name='recipe_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
