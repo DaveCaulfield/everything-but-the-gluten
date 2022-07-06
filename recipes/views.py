c3b5ef831c80c10c916a2d565a0d1995dca482cd
DaveCaulfield <dave.caulfield@gmail.com>
2022-07-06 15:25:22 +0000

    add RecipeLike view for like fetaure

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Recipe
 from .forms import CommentForm
 
 
 class RecipeList(generic.ListView):
     model = Recipe
-    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')  
     template_name = 'index.html'
     paginate_by = 6
-
 
 
 class RecipeDetail(View):
@@ -66,3 +66,15 @@ class RecipeDetail(View):
         )       
 
 
+class RecipeLike(View):
+
+    def post(self, request, slug, *args, **kwargs):
+        recipe = get_object_or_404(Recipe, slug=slug)
+
+        if recipe.likes.filter(id=request.user.id).exists():
+            recipe.likes.remove(request.user)
+        else:
+            recipe.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+        
